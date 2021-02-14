68a7f137cec9f2f406a0a9644dfd00fda6b0f0a4
nielsproest <nielsproest@hotmail.com>
2021-02-14 22:19:30 +0100

    Add files via upload

--- visual.py
+++ visual.py
@@ -21,27 +21,30 @@ cnt = {
 }
 
 #gene = "DLGAP5"
-gene = input("Give gene: ")
+while(True):
+	plt.clf()
+	gene = input("Give gene: ")
 
-for i, v in enumerate(dt[gene]):
-	label = dt.loc[i]["PAM50_Label"]
-	cnt[label].append(v)
+	for i, v in enumerate(dt[gene]):
+		label = dt.loc[i]["PAM50_Label"]
+		cnt[label].append(v)
 
-for i,k in enumerate(cnt):
-	first = False
-	for v in cnt[k]:
-		plt.scatter(i, v, label=k if not first else None, color=TBL[k], 
-					marker= "*", s=30) 
-		first = True
+	for i,k in enumerate(cnt):
+		first = False
+		for v in cnt[k]:
+			plt.scatter(i, v, label=k if not first else None, color=TBL[k], 
+						marker= "*", s=30) 
+			first = True
 
-# x-axis label 
-plt.xlabel('x - axis') 
-# frequency label 
-plt.ylabel('y - axis') 
-# plot title 
-plt.title("{} gene plot!".format(gene)) 
-# showing legend 
-plt.legend() 
-  
-# function to show the plot 
-plt.show()
+	# x-axis label 
+	plt.xlabel('x - axis') 
+	# frequency label 
+	plt.ylabel('y - axis') 
+	# plot title 
+	plt.title("{} gene plot!".format(gene)) 
+	# showing legend 
+	plt.legend() 
+
+	# function to show the plot 
+	#plt.show()
+	plt.savefig("gene_{}.png".format(gene))
